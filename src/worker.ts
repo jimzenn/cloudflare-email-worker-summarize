0ed9173e37cfbe0da2f1988688a5a5e6d6e06834
Jim Zenn <jimzenn0@gmail.com>
2025-02-06 02:15:08 -0800

    Add sendTelegramMessge function to send a message to myself.

--- src/worker.ts
+++ src/worker.ts
@@ -4,6 +4,7 @@ interface Env {
   PUSHOVER_API_KEY: string;
   PUSHOVER_USER_KEY: string;
   PUSHOVER_API_URL: string;
+  TELEGRAM_BOT_TOKEN: string;
 }
 
 
@@ -23,11 +24,36 @@ async function sendPushoverNotification(title: string, message: string, env: Env
   }
 }
 
+
+async function sendTelegramMessage(
+  chatId: string | number,
+  text: string,
+  env: Env,
+): Promise<void> {
+  const url = `https://api.telegram.org/bot${env.TELEGRAM_BOT_TOKEN}/sendMessage`
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      chat_id: chatId,
+      text: text.slice(0, 4096),
+    }),
+  });
+
+  if (!response.ok) {
+    console.error('Error sending notification:', response.status, await response.text());
+  }
+}
+
 export default {
   async email(message: ForwardableEmailMessage, env: Env, ctx: ExecutionContext): Promise<void> {
     const email = await PostalMime.parse(message.raw);
+    const chatId = 151667449;
 
     await sendPushoverNotification(email.subject, email.text, env);
+    await sendTelegramMessage(chatId, email.text, env);
   },
 };
 
