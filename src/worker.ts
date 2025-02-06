7e8a76f7932f10531c55c6d38d96dbaed9584abc
Jim Zenn <jimzenn0@gmail.com>
2025-02-06 02:02:18 -0800

    Use PostalMime to parse email.

--- src/worker.ts
+++ src/worker.ts
@@ -1,29 +1,21 @@
+import PostalMime from 'postal-mime';
+
 interface Env {
   PUSHOVER_API_KEY: string;
   PUSHOVER_USER_KEY: string;
   PUSHOVER_API_URL: string;
 }
 
-async function sendPushoverNotification(email: ForwardableEmailMessage, env: Env): Promise<void> {
-  const title = email.headers.get('subject')?.trim() || 'No Subject';
-  const content = [
-    `From: ${email.from}`,
-    `To: ${email.to}`,
-    `Subject: ${title}`,
-  ].join('\n');
 
-  const params = new URLSearchParams({
-    token: env.PUSHOVER_API_KEY,
-    user: env.PUSHOVER_USER_KEY,
-    title,
-    message: content,
-  });
-
-  console.log('Forwarding email to Pushover with params:', params.toString());
-
+async function sendPushoverNotification(title: string, message: string, env: Env): Promise<void> {
   const response = await fetch(env.PUSHOVER_API_URL, {
     method: 'POST',
-    body: params,
+    body: new URLSearchParams({
+      token: env.PUSHOVER_API_KEY,
+      user: env.PUSHOVER_USER_KEY,
+      title: title,
+      message: message,
+    }),
   });
 
   if (!response.ok) {
@@ -33,8 +25,9 @@ async function sendPushoverNotification(email: ForwardableEmailMessage, env: Env
 
 export default {
   async email(message: ForwardableEmailMessage, env: Env, ctx: ExecutionContext): Promise<void> {
-    console.log('Received email:', message);
-    await sendPushoverNotification(message, env);
+    const email = await PostalMime.parse(message.raw);
+
+    await sendPushoverNotification(email.subject, email.text, env);
   },
 };
 
