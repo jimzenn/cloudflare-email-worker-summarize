43d82dbed29c048cef65a06f8b9495037da28e38
Jim Zenn <jimzenn0@gmail.com>
2025-02-11 01:08:04 -0800

    Update message styling.

--- src/utils/telegram.ts
+++ src/utils/telegram.ts
@@ -17,9 +17,11 @@ export async function sendTelegramMessage(sender: string, subject: string, text:
   console.log('Sending Telegram message:', shortenedText);
 
   const msg = [
-    format.bold(escapeMarkdownV2(subject)),
-    "from: " + format.bold(escapeMarkdownV2(sender)),
-    format.blockquote(shortenedText)
+    format.blockquote([
+      format.bold(escapeMarkdownV2(subject)),
+      "from: " + format.code(escapeMarkdownV2(sender))
+    ].join('\n')),
+    shortenedText
   ].join('\n\n');
 
   try {
