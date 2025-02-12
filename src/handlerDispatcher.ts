19284fbe28ec1ade94a9d3708819d8eddfc0c7e3
Jim Zenn <jimzenn0@gmail.com>
2025-02-12 00:21:25 -0800

    Enable FlightHandler.

--- src/handlerDispatcher.ts
+++ src/handlerDispatcher.ts
@@ -1,8 +1,10 @@
 import { ForwardableEmailMessage } from "postal-mime";
 import { Env } from "./types/env";
+import { FlightHandler } from "./handlers/flight";
 
 export async function emailHandlerDispatcher(email: ForwardableEmailMessage, category: string, domainKnowledges: string[], env: Env) {
   if (category === "flight") {
     const flightHandler = new FlightHandler(email, domainKnowledges, env);
+    await flightHandler.handle();
   }
 }
